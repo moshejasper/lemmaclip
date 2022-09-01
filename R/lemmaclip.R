254a83b40a115a4d63210225f42a44c1ed0c7b85
moshejasper <moshe.jasper@unimelb.edu.au>
2022-09-01 12:59:03 +1000

    taxiing

--- R/lemmaclip.R
+++ R/lemmaclip.R
@@ -1,3 +1,13 @@
-lemmaclip <- function(tab){
-  print(tab)
+#' Filter lemmas to clipboard
+#'
+#' @param lower positive int. lowest NT vocab freq. to include.
+#' @param upper positive int. highest NT vocab freq. to include.
+#'
+#' @return clips information to clipboard. no important return.
+#' @export
+#'
+#' @examples
+#' lemmaclip(25)
+lemmaclip <- function(lower, upper = 50000){
+  print(lower)
 }
